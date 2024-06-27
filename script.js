dad8cc8d447c4909c0374572137501f1ac871cae
Baha Brahim <bahbouhabrahim2@gmail.com>
2024-06-26 17:25:38 -0700

    the newPage button is functional and the project is done

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-
+let container = document.querySelector(".container");
 function setNewGrid(dimension) {
-    let container = document.querySelector(".container");
-    for (let i = 0 ; i<dimension*dimension; i++) {
+    for (let i = 0; i < dimension * dimension; i++) {
         let div = document.createElement("div");
-        div.setAttribute("id" , "square" + (i+1));
-        div.setAttribute("class" , "square");
+        div.setAttribute("id", "square" + (i + 1));
+        div.setAttribute("class", "square");
         div.style.border = "solid 1px black";
         div.style.height = "57px";
         div.style.width = "50px";
-        container.appendChild(div);   
-        
-        div.addEventListener("mouseover" , () => {
+        container.appendChild(div);
+
+        div.addEventListener("mouseover", () => {
             div.style.backgroundColor = "pink";
         });
     }
 };
-window.onload  = () => {
+window.onload = () => {
     setNewGrid(16);
 }
 
 let newPageSpecs = document.querySelector(".newPageSpecs");
-newPageSpecs.addEventListener("click" , () => {
-    let dimension = prompt("Please provide dimensions of the new page : ")
-    setNewGrid(dimension);
+newPageSpecs.addEventListener("click", () => {
+    container.innerHTML = "";
+    let dimension;
+    do {
+        dimension = prompt("Please provide dimensions of the new page : ");
+    } while (dimension <= 0 || dimension > 60);
+    setNewGrid((dimension == 0) ? 16 : dimension);
 });
+
+let eraser = document.querySelector(".eraser");
+eraser.addEventListener("click" , () => {
+    let divs = document.querySelectorAll(".square");
+    divs.forEach(div => {
+        div.addEventListener("mouseover" , () => {
+            div.style.backgroundColor = "white";
+        })
+    })
+})
